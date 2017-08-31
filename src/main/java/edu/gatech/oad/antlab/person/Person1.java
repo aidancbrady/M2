7a1c2c0d8d28d7d2c010b5c4e0e0d9c9dc69e461
aidancbrady <me@aidancbrady.com>
2017-08-31 17:05:09 -0400

    Added independent changes

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,14 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	    if (input == null) {
+	        return null;
+	    }
+	    else if (input.length() <= 2) {
+	        return input;
+	    }
+	    
+	    return input.substring(2, input.length()) + input.substring(0, 2);
 	}
 	
 	/**
