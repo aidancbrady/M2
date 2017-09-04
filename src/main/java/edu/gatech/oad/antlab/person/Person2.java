b7477e553f6d873a5977ba9e750a5855ecfb4e05
Priscylla Snow <prsnow1990@gmail.com>
2017-09-04 19:15:41 -0400

    Implemented Person2
    Person2's calc() takes in a string and spits out the string randomized.

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,24 +1,24 @@
 package edu.gatech.oad.antlab.person;
-
+import java.util.Random;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
- * @author Bob
+ * @author Scylla Snow
  * @version 1.1
  */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
-	 	/**
+	/** Holds the persons real name */
+	private String name;
+	/**
 	 * The constructor, takes in the persons
 	 * name
-	 * @param pname the person's real name
+	 * @param name the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String name) {
+		this.name = name;
+	}
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -28,20 +28,43 @@ public class Person2 {
 	 *
 	 * @param input the string to be modified
 	 * @return the modified string
+	 * @throws IllegalArgumentException if input is null
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		//Person 2 put your implementation here
+		Random rand = new Random();
+		String output = "";
+		int change;
+		if (input == null) {
+			throw new IllegalArgumentException("There was no input");
+		} else if (input.length() == 1) {
+			return input;
+		}
+		while (input.length() > 0) {
+			change = rand.nextInt(input.length());
+			output = output + input.charAt(change);
+			input = input.substring(0, change) + input.substring(change + 1);
+		}
+
+		return output;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return "Original name: " + name + " \nRandom Name: " + calc(input);
 	}
+	public String getName() {
+		return name;
+	}
+	public String setName(String name) {
+		return this.name = name;
+	}
+
+
 }
