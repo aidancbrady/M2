a0da7c424fceed0f10a344a3be30b008d07da8e8
vfanelle <vfanelle@gmail.com>
2017-09-06 08:42:38 -0400

    Fixed compile time error in Person4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -32,7 +32,7 @@ public class Person4 {
     private String calc(String input) {
       //Person 4 put your implementation here
         if (input == null) {
-            throw IllegalArgumentException("No word given");
+            return "";
         }
         String word = "";
         for (int i = 0; i < input.length(); i++) {
